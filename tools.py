2030f538fe99629df4487d17f1e75d7c01f80ad0
Dhairya206 <105799152+Dhairya206@users.noreply.github.com>
2026-01-06 10:56:29 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -2,18 +2,19 @@ import yt_dlp
 from PIL import Image
 import os
 
-def download_yt(url, quality='best'):
+def download_yt(url, quality='720'):
+    # quality can be 144, 360, 720, 1080, 2160 (4K)
     ydl_opts = {
-        'format': f'bestvideo[height<={quality[:-1]}]+bestaudio/best' if 'p' in quality else 'best',
+        'format': f'bestvideo[height<={quality}]+bestaudio/best',
         'outtmpl': 'downloads/%(title)s.%(ext)s',
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         return ydl.prepare_filename(info)
 
-def images_to_pdf(image_list, output_path):
-    images = [Image.open(img).convert('RGB') for img in image_list]
-    if images:
-        images[0].save(output_path, save_all=True, append_images=images[1:])
+def images_to_pdf(image_list, output_path="output.pdf"):
+    imgs = [Image.open(i).convert('RGB') for i in image_list]
+    if imgs:
+        imgs[0].save(output_path, save_all=True, append_images=imgs[1:])
         return output_path
     return None
