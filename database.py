3669ddada5d93ba1b8be79f2db3d69b6369b3300
Dhairya206 <105799152+Dhairya206@users.noreply.github.com>
2026-01-06 11:08:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,4 @@
-import pymongo
-
-# MongoDB connection (UserLAnd mein local ya MongoDB Atlas use karein)
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["railway_bot"]
-tickets_col = db["tickets"]
-
-def save_ticket(user_id, issue):
-    ticket_data = {"user_id": user_id, "issue": issue, "status": "Open"}
-    result = tickets_col.insert_one(ticket_data)
-    return result.inserted_id
-
-def get_all_tickets():
-    return list(tickets_col.find())
+python-telegram-bot==13.15
+pymongo
+python-dotenv
+media-tools
